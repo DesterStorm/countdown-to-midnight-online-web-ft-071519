b5ede0b7d4ca6ea7315f22ad50fbed1bb43b9038
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-18 22:28:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
-
+countdown = 0
 def countdown
+  
+  while countdown < 5 do 
+    puts "#{countdown} SECOND(S)!"
+    countdown += 1 
+  end
+  
+  when countdown = 5
+    puts "HAPPY NEWYEAR!"
+  end
+  
 end
